853cc052d98f5cbc764d2eeab83bae340f78a67d
Côme Richetta <crichetta@rubika-edu.com>
2022-04-27 10:54:48 +0200

    Crates for item spawn

---
+++ SI_StarryBloom/Assets/Scripts/Arena/Crate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    public List<GameObject> spawnItems;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            CrateExplode();
+        }
+    }
+
+    public void CrateExplode()
+    {
+        Instantiate(spawnItems[Random.Range(0, spawnItems.Count)], transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+
+
+}
--- SI_StarryBloom/Assets/Scripts/Arena/ItemSpawn.cs
+++ SI_StarryBloom/Assets/Scripts/Arena/ItemSpawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ItemSpawn : MonoBehaviour
 {
     public List<GameObject> spawnPoints;
-    public List<GameObject> spawnItems;
+    public GameObject spawnedCrate;
 
     public GameObject spawnTarget;
 
@@ -17,21 +17,11 @@ public class ItemSpawn : MonoBehaviour
         Spawn(); //why tf does it not wait for the coroutine tho?
     }
 
-    // UPDATE ONLY USED FOR DEBUG, DELETE IN FINAL BUILD
-    /*public void Update()
-    {
-        if (Input.GetKeyDown ("down"))
-        {
-            Spawn();
-        }
-    }*/
-
     private void Spawn()
     {
         StartCoroutine(SpawnTimer());
-        var clone = Instantiate(spawnItems[Random.Range(0, spawnItems.Count)], spawnPoints[Random.Range(0, spawnPoints.Count)].transform.position, spawnPoints[Random.Range(0, spawnPoints.Count)].transform.rotation);
+        var clone = Instantiate(spawnedCrate, spawnPoints[Random.Range(0, spawnPoints.Count)].transform.position, spawnPoints[Random.Range(0, spawnPoints.Count)].transform.rotation);
         clone.GetComponent<Rigidbody>().AddForce((spawnTarget.transform.position - clone.transform.position) * Random.Range(minThrowStrength, maxThrowStrength));
-        //Debug.Log("YEET!");
     }
 
     private IEnumerator SpawnTimer()
