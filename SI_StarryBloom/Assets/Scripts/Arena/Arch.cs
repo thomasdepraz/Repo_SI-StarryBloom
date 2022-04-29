1e31988e7a31c95392ac840a72ee062838275d8b
Noé <noe.blanc.2k@gmail.com>
2022-04-29 15:56:02 +0200

    little fix arch script

--- SI_StarryBloom/Assets/Scripts/Arena/Arch.cs
+++ SI_StarryBloom/Assets/Scripts/Arena/Arch.cs
@@ -22,11 +22,11 @@ public class Arch : MonoBehaviour
 
         if(timer <= 0 && archSpawned == false)
         {            
-            int i = Random.Range(0, archPositions.Count - 1);
+            int i = Random.Range(0, archs.Count - 1);
             archs[i].SetActive(true);
             archs.RemoveAt(i);
 
-            int j = Random.Range(0, archPositions.Count - 1);
+            int j = Random.Range(0, archs.Count - 1);
             archs[j].SetActive(true);            
 
             archSpawned = true;
